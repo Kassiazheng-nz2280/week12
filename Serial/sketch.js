6d9293478ab40ce361379a81627f5ed934fa7a7f
Kassia Zheng <nz2280@nyu.edu>
2023-12-04 21:48:22 -0500

    1

--- Serial/sketch.js
+++ Serial/sketch.js
@@ -1,5 +1,3 @@
-let SERIAL_PORT = "/dev/cu.usbmodem3485187B80BC2";
-
 let mSerial;
 
 let connectButton;
@@ -7,21 +5,24 @@ let connectButton;
 let readyToReceive;
 let cBackgroundColor;
 
+let isLightOn = false;
+
 function receiveSerial() {
   let line = mSerial.readUntil("\n");
-  trim(line);
-  if (!line) return;
+  // trim(line);
+  // if (!line) return;
+  console.log(line);
 
-  if (line.charAt(0) != "{") {
-    print("error: ", line);
-    readyToReceive = true;
-    return;
-  }
+//   if (line.charAt(0) != "{") {
+//     print("error: ", line);
+//     readyToReceive = true;
+//     return;
+//   }
 
-  let data = JSON.parse(line).data;
-  let a0 = data.A0;
+//   let data = JSON.parse(line).data;
+//   let a0 = data.A0;
 
-  cBackgroundColor = map(a0.value, a0.min, a0.max, 0, 255);
+//   cBackgroundColor = map(a0.value, a0.min, a0.max, 0, 255);
   readyToReceive = true;
 }
 
@@ -45,15 +46,33 @@ function setup() {
   connectButton = createButton("Connect To Serial");
   connectButton.position(width / 2, height / 2);
   connectButton.mousePressed(connectToSerial);
+  
+  lightButton = createButton("Toggle LED");
+  lightButton.position(100, 100);
+  lightButton.mousePressed(toggleLED);
 }
+
+function toggleLED(){
+  console.log("Clicked");
+  if(isLightOn == false){
+    mSerial.write("on");
+    isLightOn = true;
+  }else{
+    mSerial.write("off");
+    isLightOn = false;
+  }
+  
+}
+
 
 function draw() {
   background(cBackgroundColor);
 
   if (mSerial.opened() && readyToReceive) {
+    
     readyToReceive = false;
     mSerial.clear();
-    mSerial.write(0xab);
+    // mSerial.write("HHello");
   }
 
   if (mSerial.availableBytes() > 0) {
